bfc9e1a96423ae753cc705883d9a2569223d5292
jotadagostin <107184368+jotadagostin@users.noreply.github.com>
2023-03-04 13:23:19 +0100

    css ready
    and starting js code

---
+++ main.js
@@ -0,0 +1,8 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationsButtons = document.querySelectorAll("[data-operator]");
+const equalsButtons = document.querySelectorAll("[data-equals]");
+const deleteButtons = document.querySelectorAll("[data-delete]");
+const previousOperand = document.querySelectorAll("[data-previous-operand]");
+const currentOperand = document.querySelectorAll("[data-current-operand]");
+
+
